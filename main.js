6d3e436e8bb2a0e55107db44f01696c1696913b1
onionskin12 <jamesonionskin@gmail.com>
2025-03-16 13:47:19 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const currentUrl = window.location.href;
-if (currentUrl.endsWith("/test")) {
-  return new Response(JSON.stringify({ "Hello": "World!" }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
-}
