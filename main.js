1e883666d13d4f69f333c9692701e6764e18e53a
onionskin12 <jamesonionskin@gmail.com>
2025-03-16 13:38:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const currentUrl = window.location.href;
 if (currentUrl.endsWith("/test")) {
-  return Response.json({
-    "Hello, ": "World!"
-  })
+  return new Response(JSON.stringify({ "Hello": "World!" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
 }
