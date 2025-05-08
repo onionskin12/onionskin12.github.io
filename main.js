b6f1d120f83a674100e62c0182c48ca562487b3d
onionskin12 <jamesonionskin@gmail.com>
2025-05-08 13:41:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,6 @@ var btn = document.getElementById("btn");
 
 btn.onclick = function() {
   if (pw.value === "test") {
-    document.innerHTML = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>`;
+    window.location = "https://google.com";
     }
 }
