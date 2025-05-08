43a9bd4c211fa6438580bce0f0f2ddf258d17e1a
onionskin12 <jamesonionskin@gmail.com>
2025-05-08 14:01:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,7 @@ var pw = document.getElementById("password");
 var btn = document.getElementById("btn");
 
 btn.onclick = function() {
-  if (pw.value === "test") {
-    document.innerHTML = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-</body>
-</html>
-    `
+  if (pw.value === "1337") {
+    window.location = "https://";
   }
 }
