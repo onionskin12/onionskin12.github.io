7d09818f37abd8f8ac5e76463bb0501fdb1ae714
onionskin12 <jamesonionskin@gmail.com>
2025-05-09 15:58:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var pw = document.getElementById("password");
 var btn = document.getElementById("btn");
 
 btn.onclick = function() {
-  if (pw.value === "ет я нез") {
+  if (pw.value === "1337") {
     window.location = "https://docs.google.com/document/d/1V0mJcpBtra6AjezoDGmD9coh95KgD61Xrf35pkRRakY/edit?usp=sharing";
   }
 }
